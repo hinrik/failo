2342dc08c2750eca037d0d84b9041b3431b742a8
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-08-16 22:31:58 +0000

    script to retrain failo.sqlite on reboot

---
+++ utils/retrain-on-reboot.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Exit on errors
+trap 'fail' ERR
+fail () {
+    code=$?
+    echo "Failed with exit code $code"
+    exit 1
+}
+
+log='/home/avar/.irssi/logs/freenode/#avar.log '
+brain=/var/tmp/ramdisk/failo/failo.sqlite
+ok=/var/tmp/ramdisk/failo/failo.trained
+dir="$(dirname $brain)"
+trn="$dir/avar.trn"
+
+mkdir -p $dir
+
+echo "Training from '$log' to $trn"
+pv "$log" | irchailo-seed -f irssi -b failo -n failo -r '^,\w' >$trn
+
+echo "Creating a new brain at $brain"
+hailo \
+    --brain $brain \
+    --train $trn \
+    --order 4    
+
+# Indicate that we're done training
+echo "Done training, creating $ok"
+>$ok
