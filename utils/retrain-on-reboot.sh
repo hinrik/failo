7b02c585152ff5f98a5170698814e16fdbb74ff7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-08-17 13:48:23 +0000

    utils/retrain-on-reboot.sh: train from #avar, #failo and #hailo

--- utils/retrain-on-reboot.sh
+++ utils/retrain-on-reboot.sh
@@ -8,21 +8,26 @@ fail () {
     exit 1
 }
 
-log='/home/avar/.irssi/logs/freenode/#avar.log '
 brain=/var/tmp/ramdisk/failo/failo.sqlite
 ok=/var/tmp/ramdisk/failo/failo.trained
 dir="$(dirname $brain)"
-trn="$dir/avar.trn"
 
 mkdir -p $dir
+mkdir -p $dir/trn
 
-echo "Training from '$log' to $trn"
-pv "$log" | irchailo-seed -f irssi -b failo -n failo -r '^,\w' >$trn
+for log in '/home/avar/.irssi/logs/freenode/#avar.log ' '/home/avar/.irssi/logs/freenode/#failo.log ' '/home/avar/.irssi/logs/freenode/#hailo.log '; do
+    short=$(echo $log | sed 's/.*\#//; s/\.log\s*//')
+    to=$dir/trn/$short.trn
+    echo "Training from '$log' to $to"
+    pv "$log" | irchailo-seed -f irssi -b failo -n failo -r '^,\w' >$to
+done
+
+pv $dir/trn/*.trn > $dir/trn/all.trn
 
 echo "Creating a new brain at $brain"
 hailo \
     --brain $brain \
-    --train $trn \
+    --train $dir/trn/all.trn \
     --order 2
 
 # Indicate that we're done training
